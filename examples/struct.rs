88fe346c7db29259f4db5a070c6e83225033f1ed
CedarHuang <cedar.hxd@gmail.com>
2021-02-24 12:47:37 +0800

    添加Adrop包装类

---
+++ examples/struct.rs
@@ -0,0 +1,22 @@
+extern crate adrop;
+
+use adrop::*;
+
+struct Test {}
+
+impl Drop for Test {
+    fn drop(&mut self) {
+        println!(
+            "Dropping HasDrop! ThreadId: {:?}",
+            std::thread::current().id()
+        );
+    }
+}
+
+fn main() {
+    println!("Main ThreadId: {:?}", std::thread::current().id());
+    let _ = Adrop::new(Test {});
+    // Output:
+    // Main ThreadId: ThreadId(1)
+    // Dropping HasDrop! ThreadId: ThreadId(2)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 extern crate lazy_static;
 
 use std::{
+    mem::ManuallyDrop,
+    ops::{Deref, DerefMut},
     sync::{
         mpsc::{channel, Sender},
         Mutex,
@@ -26,12 +28,52 @@ lazy_static! {
     static ref TX: Mutex<TrashSender> = Mutex::new(run());
 }
 
-pub fn adrop<T>(trash: T)
-where
-    T: Send + 'static,
-{
+pub fn adrop<T: Send + 'static>(trash: T) {
     let _ = TX
         .lock()
         .expect("Theoretically will not fail")
         .send(Box::new(trash));
+}
+
+#[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Adrop<T: Send + 'static> {
+    content: Option<ManuallyDrop<T>>,
+}
+
+impl<T: Send> Adrop<T> {
+    pub fn new(content: T) -> Adrop<T> {
+        Adrop::<T> {
+            content: Some(ManuallyDrop::new(content)),
+        }
+    }
+
+    pub fn into_inner(mut self) -> T {
+        let content = unsafe { ManuallyDrop::take(self.content.as_mut().unwrap()) };
+        self.content = None;
+        content
+    }
+}
+
+impl<T: Send> Deref for Adrop<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.content.as_ref().unwrap()
+    }
+}
+
+impl<T: Send> DerefMut for Adrop<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.content.as_mut().unwrap()
+    }
+}
+
+impl<T: Send> Drop for Adrop<T> {
+    fn drop(&mut self) {
+        if let Some(content) = self.content.as_mut() {
+            unsafe {
+                adrop(ManuallyDrop::take(content));
+            }
+        }
+    }
 }
